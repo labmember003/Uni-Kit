73c5900464bc220e196a38b86299038a0c5eb932
Avi <avi@Anirudhs-MacBook-Pro-2.local>
2024-01-30 14:08:54 +0530

    added bottom sheet instead of modal drawer

--- app/src/main/java/com/falcon/unikit/screens/MainScreen.kt
+++ app/src/main/java/com/falcon/unikit/screens/MainScreen.kt
@@ -22,18 +22,18 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.DrawerState
-import androidx.compose.material.DrawerValue
-import androidx.compose.material.ModalDrawer
-import androidx.compose.material.rememberDrawerState
 import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -50,9 +50,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
-import com.airbnb.lottie.compose.rememberLottieComposition
 import com.falcon.unikit.NavDrawerContent
 import com.falcon.unikit.R
 import com.falcon.unikit.models.item.YearItem
@@ -70,50 +67,45 @@ fun MainScreen(yearList: State<List<YearItem>>, navController: NavHostController
 
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ModalDrawerSample(
     yearList: State<List<YearItem>>,
     navController: NavHostController,
     navigateToBranchScreen: (yearID: String) -> Unit
 ) {
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
-    rememberCoroutineScope()
-
-    ModalDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            DrawerContent(navController)
-        },
-        content = {
-            ChooseYearScreen(yearList, drawerState, navigateToBranchScreen)
-        }
+    val modalSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmValueChange = { it != ModalBottomSheetValue.HalfExpanded },
+        skipHalfExpanded = true
     )
+    ModalBottomSheetLayout(
+        sheetState = modalSheetState,
+        sheetShape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
+        sheetContent = {
+            MainScreenBottomSheetContent(modalSheetState, navController)
+        }
+    ) {
+        ChooseYearScreen(yearList, navigateToBranchScreen, modalSheetState)
+    }
 }
 
-
-@Preview(showBackground = true, showSystemUi = true)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun _ChooseYearScreen() {
-    val yearlist = remember {
-        mutableStateOf(
-            listOf(
-                YearItem("123", "First"),
-                YearItem("123", "Second"),
-                YearItem("123", "Third"),
-                YearItem("123", "Fourth")
-            )
-        )
-    }
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    ChooseYearScreen(yearlist, drawerState) {
-
-    }
+fun MainScreenBottomSheetContent(
+    modalSheetState: ModalBottomSheetState,
+    navController: NavHostController
+) {
+    DrawerContent(navController, modalSheetState)
 }
+
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ChooseYearScreen(
     yearList: State<List<YearItem>>,
-    drawerState: DrawerState,
-    navigateToBranchScreen: (yearID: String) -> Unit
+    navigateToBranchScreen: (yearID: String) -> Unit,
+    modalSheetState: ModalBottomSheetState
 ) {
     val scope = rememberCoroutineScope()
     Column(
@@ -122,7 +114,7 @@ fun ChooseYearScreen(
             .padding(24.dp)
             .verticalScroll(rememberScrollState())
     ) {
-        MainScreenHeader(scope, drawerState)
+        MainScreenHeader(scope, modalSheetState)
         Spacer(modifier = Modifier.height(16.dp))
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -252,10 +244,11 @@ fun ChooseYearScreen(
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun MainScreenHeader(
     scope: CoroutineScope,
-    drawerState: DrawerState
+    modalSheetState: ModalBottomSheetState,
 ) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -278,7 +271,7 @@ private fun MainScreenHeader(
                 .size(23.dp)
                 .clickable {
                     scope.launch {
-                        drawerState.open()
+                        modalSheetState.show()
                     }
                 }
         )
@@ -417,29 +410,39 @@ fun getImageFromYear(yearName: String): Int {
     return R.drawable.graduation
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun DrawerContent(navController: NavHostController) {
-    val context = LocalContext.current
+fun DrawerContent(navController: NavHostController, modalSheetState: ModalBottomSheetState) {
+    val scope = rememberCoroutineScope()
     Column(
         verticalArrangement = Arrangement.spacedBy(0.dp),
         modifier = Modifier
             .fillMaxHeight()
         ) {
-        val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.productivity))
-        com.airbnb.lottie.compose.LottieAnimation(
-            composition = composition,
-            iterations = LottieConstants.IterateForever,
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .size(300.dp)
-        )
+                .padding(bottom = 16.dp)
+            ,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            androidx.compose.material3.Text(
+                text = "Menu",
+                style = androidx.compose.material.MaterialTheme.typography.subtitle1,
+                fontSize = 20.sp
+            )
+            androidx.compose.material.Icon(
+                Icons.Filled.Close,
+                contentDescription = "Close",
+                modifier = Modifier
+                    .clickable {
+                        scope.launch { modalSheetState.hide() }
+                    }
+            )
+        }
         NavDrawerContent("Profile", R.drawable.baseline_person_24) {
             navController.navigate("profile")
         }
-//        Spacer(modifier = Modifier.height(2.dp))
-//        NavDrawerContent("My Notes", R.drawable.baseline_sticky_note_2_24) {
-//            navController.navigate("my_notes")
-//        }
         Spacer(modifier = Modifier.height(2.dp))
         NavDrawerContent("Settings", R.drawable.baseline_settings_24) {
             navController.navigate("settings")
